78a9f8a1c5db977521dd15a0b7458ac6788759b3
Gurkirat Singh Arora <gsa119@sfu.ca>
2023-07-29 12:05:19 +0530

    added apilimiter to register and login route

--- starter/routes/auth.js
+++ starter/routes/auth.js
@@ -6,7 +6,7 @@ const authenticateUser = require('../middleware/authentication')
 const rateLimiter = require('express-rate-limit');
 const apiLimiter = rateLimiter({
     windowMs: 15 * 60 * 1000,
-    max: 1,
+    max: 10,
     message: {
         msg: 'Too many request from this IP, please try again after 15 minutes'
     }
