d4dac7d1c368cc4e0fe6d118b9960c55950dc808
Gurkirat Singh Arora <gsa119@sfu.ca>
2023-07-29 12:03:07 +0530

    Restricting CRUD functionality for testUser

--- starter/middleware/authentication.js
+++ starter/middleware/authentication.js
@@ -13,7 +13,8 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name }
+    const testUser = payload.userId === '64c431ea19b4931b599f5ce1';
+    req.user = { userId: payload.userId, testUser }
     next()
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')
--- starter/routes/auth.js
+++ starter/routes/auth.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const router = express.Router()
-
+const testUser = require('../middleware/testUser');
 const authenticateUser = require('../middleware/authentication')
+
+const rateLimiter = require('express-rate-limit');
+const apiLimiter = rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 1,
+    message: {
+        msg: 'Too many request from this IP, please try again after 15 minutes'
+    }
+})
 const { register, login, updateUser } = require('../controllers/auth')
-router.post('/register', register)
-router.post('/login', login)
-router.patch('/updateUser', authenticateUser, updateUser)
+router.post('/register', apiLimiter, register)
+router.post('/login', apiLimiter, login)
+router.patch('/updateUser', authenticateUser, testUser, updateUser)
 
 module.exports = router
